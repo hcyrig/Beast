f8ad2d489b4435fe61cf32f63369fa7cbd6b92ef
Kostiantyn Girych (hcyrig) <hcyrig@gmail.com>
2016-12-13 12:54:08 +0200

    update sms, email, shared Action Manager

--- Beast/classes/Managers/ActionsManager.swift
+++ Beast/classes/Managers/ActionsManager.swift
@@ -22,7 +22,7 @@ public class ActionsManager: NSObject, MFMessageComposeViewControllerDelegate, M
     // MARK: Phone
     
     public func callURLFromPhoneString(phone: String) -> NSURL? {
-    
+        
         let charsSet = CharacterSet(charactersIn: "0123456789-+()").inverted
         let cleanedString = phone.components(separatedBy: charsSet).joined(separator: "")
         let telURL = NSURL(string:"tel://\(cleanedString)")
@@ -38,34 +38,74 @@ public class ActionsManager: NSObject, MFMessageComposeViewControllerDelegate, M
         UIAlertView(title: alertTitle, message: alertMessage, delegate: nil, cancelButtonTitle: "Cancel", otherButtonTitles: "Call").show()
     }
     
+    public func share(sender:UIView, objects:[AnyObject],
+                      controller:UIViewController,
+                      animated:Bool = true,
+                      types:[UIActivityType] = [
+        .postToFacebook,
+        .postToTwitter,
+        .postToWeibo,
+        .message,
+        .mail,
+        .print,
+        .copyToPasteboard,
+        .assignToContact,
+        .saveToCameraRoll,
+        .addToReadingList,
+        .postToFlickr,
+        .postToVimeo,
+        .postToTencentWeibo,
+        .airDrop ]) {
+        
+        let objectsToShare = objects
+        let activityVC = UIActivityViewController(activityItems: objectsToShare,
+                                                  applicationActivities: nil)
+        
+        activityVC.excludedActivityTypes = types
+        activityVC.popoverPresentationController?.sourceView = sender
+        controller.present(activityVC,
+                           animated: animated,
+                           completion: nil)
+    }
     
-    //MARK: send sms
-    
-    public func sendSMSTo(phone: String) {
+    public func send(sms:String = "",
+                     recipients:[String] = []) {
         
         if MFMessageComposeViewController.canSendText() == false {
             
-            UIAlertView.showWithTitle(title: NSLocalizedString("Error", comment: ""), message: NSLocalizedString("Device can not send text messages", comment: ""))
+            UIAlertView.showWithTitle(title: NSLocalizedString("Error",
+                                                               comment: ""),
+                                      message: NSLocalizedString("Device can not send text messages",
+                                                                 comment: ""))
             return
         }
         
         let messageController = MFMessageComposeViewController()
         messageController.messageComposeDelegate = ActionsManager.sharedInstance
-        messageController.recipients = [phone]
-        UIApplication.shared.keyWindow?.rootViewController?.present(messageController, animated: true, completion: nil)
+        messageController.recipients = recipients
+        messageController.body = sms
+        UIApplication.shared.keyWindow?.rootViewController?.present(messageController,
+                                                                    animated: true,
+                                                                    completion: nil)
     }
     
-    public func emailTo(recipients: [String]) {
-    
+    public func send(email:String, recipients:[String]) {
+        
         if MFMailComposeViewController.canSendMail() == false {
             
-            UIAlertView.showWithTitle(title: NSLocalizedString("Error", comment: ""), message: NSLocalizedString("No email account found on this device. Please setup your email account on your device to use this service", comment: ""))
+            UIAlertView.showWithTitle(title: NSLocalizedString("Error",
+                                                               comment: ""),
+                                      message: NSLocalizedString("No email account found on this device. Please setup your email account on your device to use this service",
+                                                                 comment: ""))
             return
         }
-    
+        
         let mailComposeVC = MFMailComposeViewController()
         mailComposeVC.setToRecipients(recipients)
-        UIApplication.shared.keyWindow?.rootViewController?.present(mailComposeVC, animated: true, completion: nil)
+        mailComposeVC.setMessageBody(email, isHTML: true)
+        UIApplication.shared.keyWindow?.rootViewController?.present(mailComposeVC,
+                                                                    animated: true,
+                                                                    completion: nil)
         mailComposeVC.mailComposeDelegate = ActionsManager.sharedInstance
     }
     
@@ -85,9 +125,9 @@ public class ActionsManager: NSObject, MFMessageComposeViewControllerDelegate, M
     
     
     // MARK: - open google maps from application by coordinates (lat, lng)
-
-    public func openGoogleMaps(point:CGPoint?, shemas:String, name:String) {
     
+    public func openGoogleMaps(point:CGPoint?, shemas:String, name:String) {
+        
         guard let p = point else {
             print("point can't be nil")
             return
