5785fdcb44584e55e66a504754b1b0b3cf8bbc00
Kostiantyn Girych <hcyrig@gmail.com>
2017-02-18 14:46:14 +0200

    fix for location manager implementation

--- Beast/classes/Managers/LocationManager.swift
+++ Beast/classes/Managers/LocationManager.swift
@@ -9,146 +9,125 @@
 import CoreLocation
 import Foundation
 
-
 public let LocationManagerDidUpdateLocationNotification = "LocationManagerDidUpdateLocationNotification"
 public let LocationManagerDidUpdateHeadingNotification = "LocationManagerDidUpdateHeadingNotification"
 
-public let kDidAskToTurnOnLocation = "kDidAskToTurnOnLocation"
-
 public class LocationManager: NSObject, CLLocationManagerDelegate {
     
-    public static let sharedInstance = LocationManager()
+    public static let shared = LocationManager()
+    private(set) var manager: CLLocationManager!
     
     private override init() {
         super.init()
+        
+        self.manager = CLLocationManager()
+        self.manager.delegate = self
+        self.manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        self.manager.distanceFilter = 5
     }
     
     
     // MARK: Properties
     
     public var location: CLLocation?
-    private(set) var locationManager: CLLocationManager!
-    private(set) var simulationMode: Bool = false
     private(set) var heading: CLHeading?
-    var locationChangedDistanceToNotify: CLLocationDistance = 50
-    
-    public static var didAskToTurnOnLocation: Bool? {
-        get {
-            if let value = (BeastNOSQL.object(key: kDidAskToTurnOnLocation) as AnyObject).boolValue {
-                return value
-            }
-            return false
-        }
-        set(value) {
-            BeastNOSQL.setObject(value: value, key: kDidAskToTurnOnLocation)
-        }
-    }
-    
-    // MARK: Class functions
-    
-    public static func requestLocationPermission() {
-        
-        if (CLLocationManager.authorizationStatus() == .denied && LocationManager.didAskToTurnOnLocation != nil) {
-            
-//            let alert = UIAlertView(title: nil, message: NSLocalizedString("Please enable device Location Services", comment: ""), delegate: nil, cancelButtonTitle: nil)
-//            alert.addButtonItem(RIButtonItem.itemWithLabel(NSLocalizedString("Cancel", comment: "")) as! RIButtonItem)
-//            alert.addButtonItem(RIButtonItem.itemWithLabel(NSLocalizedString("Open Settings", comment: ""), action: { () -> Void in
-//                ActionsManager.sharedInstance.openSettings()
-//            }) as! RIButtonItem)
-//            
-//            alert.show();
-            
-            LocationManager.didAskToTurnOnLocation = true
-        }
-        
-        LocationManager.sharedInstance.startUpdatingLocation()
-    }
-    
-    public static func isAuthorized() -> Bool {
-        
-        let status: CLAuthorizationStatus = CLLocationManager.authorizationStatus()
-        if status == .notDetermined || status == .denied || status == .restricted {
-            return false
-        }
-        return true
-    }
     
     
     // MARK: Actions
     
     public func startUpdatingLocation() {
         
-        if self.locationManager == nil {
-            
-            self.locationManager = CLLocationManager()
-            self.locationManager.delegate = self
-            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            self.locationManager.distanceFilter = kCLDistanceFilterNone
-
-            self.locationManager.requestWhenInUseAuthorization()
-        }
-        
-        self.locationManager.startUpdatingLocation()
-        
-        if !CLLocationManager.locationServicesEnabled() {
-            
+        if !CLLocationManager.locationServicesEnabled()
+        || !isAuthorized() {
+            requestAutorization()
             print("location services disabled")
-            
-            self.stopUpdatingLocation()
+            return
         }
+        manager.startUpdatingLocation()
     }
     
     public func stopUpdatingLocation() {
+        manager.stopUpdatingLocation()
+    }
+
+    
+    // MARK: Class functions
+    
+    public func showManualAutorizationAlert() {
         
-        self.locationManager.stopUpdatingLocation()
+        let alert = UIAlertView(title: nil, message: NSLocalizedString("Please enable device Location Services", comment: ""), delegate: nil, cancelButtonTitle: nil)
+        
+        alert.addButtonItem(RIButtonItem.itemWithLabel(NSLocalizedString("Cancel", comment: "")) as! RIButtonItem)
+        
+        alert.addButtonItem(RIButtonItem.itemWithLabel(NSLocalizedString("Open Settings", comment: ""), action: { () -> Void in
+            ActionsManager.sharedInstance.openSettings()
+        }) as! RIButtonItem)
+        
+        alert.show()
     }
     
-    public func startUpdatingHeading() {
+    public func isAuthorized() -> Bool {
         
-        self.locationManager.startUpdatingHeading()
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways: return true
+        case .authorizedWhenInUse: return true
+        case .denied: break
+        case .notDetermined: break
+        case .restricted: break
+        }
+        return false
     }
     
-    public func stopUpdatingHeading() {
-        
-        self.locationManager.stopUpdatingHeading()
+    public func requestAutorization() {
+    
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways: break
+        case .authorizedWhenInUse: break
+        case .denied:
+            showManualAutorizationAlert()
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .restricted:
+            showManualAutorizationAlert()
+        }
     }
+
     
-    
-    // MARK: CLLocationManagerDelegate
+    // MARK: CLLocationManagerDelegate , update location information
     
     public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         
-        if LocationManager.isAuthorized() {
-            self.startUpdatingLocation()
+        if !isAuthorized() {
+            stopUpdatingLocation()
+            return
         }
-        else {
-            self.stopUpdatingLocation()
-        }
+        startUpdatingLocation()
     }
     
     public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        if let newLocation = locations.last {
-            if self.location != nil {
-                let distanceDifference = self.location!.distance(from: newLocation)
-                if distanceDifference < locationChangedDistanceToNotify {
-                    return
-                }
-            }
-            
-            self.location = newLocation
-            NotificationCenter.default.post(name: NSNotification.Name(rawValue: LocationManagerDidUpdateLocationNotification), object: nil)
-        }
+        location = manager.location
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: LocationManagerDidUpdateLocationNotification), object: nil)
     }
     
     public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error)
     }
     
+    
+    // MARK: - update heading information
+    
     public func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
 
         heading = newHeading
-        
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: LocationManagerDidUpdateHeadingNotification), object: nil)
+    }
+    
+    public func startUpdatingHeading() {
+        manager.startUpdatingHeading()
+    }
+    
+    public func stopUpdatingHeading() {
+        manager.stopUpdatingHeading()
     }
 }
